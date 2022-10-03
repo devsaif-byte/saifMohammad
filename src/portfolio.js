3038d617d4d0addea8787ca917784abba72faf67
devsaif-byte <saifuddinrabib654@gmail.com>
2022-10-03 20:47:35 +0600

    changes

--- src/portfolio.js
+++ src/portfolio.js
@@ -10,7 +10,7 @@ const about = {
   role: 'MERN Stack Developer',
   description:
     'Adipisicing sit fugit ullam unde aliquid sequi Facilis soluta facilis perspiciatis corporis nulla aspernatur. Autem eligendi rerum delectus modi quisquam? Illo ut quasi nemo ipsa cumque perspiciatis! Maiores minima consectetur.',
-  Resume: 'shorturl.at/hjUXZ',
+  resume: 'shorturl.at/hjUXZ',
   social: {
     Facebook: 'https://www.facebook.com/saifmohammad2020/',
     linkedin: 'https://www.linkedin.com/in/saif-mohammad-9995801a2/',
